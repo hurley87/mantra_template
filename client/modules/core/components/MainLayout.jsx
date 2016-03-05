a25d4868f742ea9dcbccdfb063cbfb90bbcbe39a
Tim Corcoran <tim.corcoran@utoronto.ca>
2016-03-05 17:59:31 -0500

    fixed indentation in first feature

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import{ Grid, Row, Col, PageHeader } from 'react-bootstrap';
 
 const Layout = ({content}) => (
-	<Grid>
-		<Row>
-				<Col xs={12}>
-					<PageHeader>Impossible List </PageHeader>
-				</Col>
-		</Row>
-		{content()}
-	</Grid>
+
+<Grid>
+	<Row>
+		<Col xs={12}>
+			<PageHeader>Impossible List </PageHeader>
+		</Col>
+	</Row>
+	{content()}
+</Grid>
 );
 
 export default Layout;
