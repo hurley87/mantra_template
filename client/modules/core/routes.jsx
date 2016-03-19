56b5facbe56813789f0e1d5458acf995c368c4f9
hurley87 <dhurls99@gmail.com>
2016-03-19 12:19:42 -0400

    started the inner layout for marketing pages

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 import Layout from './components/MainLayout.jsx';
+import InnerLayout from './components/InnerLayout.jsx';
 import ItemList from '../items/components/ItemList.jsx';
 
 export default function(injectDeps, {FlowRouter}) {
@@ -12,6 +13,15 @@ export default function(injectDeps, {FlowRouter}) {
     action() {
       mount(MainLayoutCtx, {
         content: () => (<ItemList />)
+      })
+    }
+  })
+
+  FlowRouter.route('/about', {
+    name: 'marketing.about',
+    action() {
+      mount(InnerLayoutCtx, {
+        content: () => (<About />)
       })
     }
   })
