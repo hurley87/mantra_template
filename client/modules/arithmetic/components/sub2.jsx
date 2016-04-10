b62a6418d10d8b573cb05e90642f9a553a8343c2
Tim Corcoran <tim.corcoran@utoronto.ca>
2016-04-09 20:08:25 -0400

    added copy to sub1, sub2, sub3

--- client/modules/arithmetic/components/sub2.jsx
+++ client/modules/arithmetic/components/sub2.jsx
@@ -6,6 +6,8 @@ class Sub2 extends React.Component {
     return (
       <div id='panel' className='Sub2'>
         <h3>Sub2</h3>
+
+        <p>Single-origin coffee chia banjo, kitsch schlitz 90's you probably haven't heard of them brooklyn asymmetrical meditation meh raw denim offal 3 wolf moon. Gochujang letterpress hoodie cold-pressed, salvia dreamcatcher pabst iPhone. Small batch brunch 90's, tacos tote bag wayfarers dreamcatcher tattooed cronut meditation ramps kickstarter. Selvage cronut lomo ethical banjo, typewriter pop-up pour-over truffaut small batch. Lomo farm-to-table flexitarian locavore four loko. Photo booth plaid umami butcher echo park. Drinking vinegar crucifix truffaut migas, fanny pack single-origin coffee master cleanse raw denim.</p>
       </div>
     )
   }
--- client/modules/arithmetic/components/sub3.jsx
+++ client/modules/arithmetic/components/sub3.jsx
@@ -6,6 +6,8 @@ class Sub3 extends React.Component {
     return (
       <div id='panel' className='Sub3'>
         <h3>Sub3</h3>
+
+        <p>Skateboard vegan fashion axe sriracha ramps ethical, trust fund migas. Gastropub williamsburg lumbersexual, beard vinyl pug food truck selvage. Aesthetic food truck photo booth post-ironic taxidermy neutra. Meggings quinoa truffaut, thundercats microdosing polaroid VHS blog. Hella locavore roof party, seitan health goth kombucha organic photo booth hammock. Mumblecore ethical mustache twee flexitarian cred. 90's PBR&B sriracha meggings.</p>
       </div>
     )
   }
