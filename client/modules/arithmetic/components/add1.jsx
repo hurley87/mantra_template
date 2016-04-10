0ce64defca82a60bb83c4922fc37ef3f4ca10209
Tim Corcoran <tim.corcoran@utoronto.ca>
2016-04-09 20:02:13 -0400

    added copy to add1, add2 and add3

--- client/modules/arithmetic/components/add1.jsx
+++ client/modules/arithmetic/components/add1.jsx
@@ -7,6 +7,8 @@ class Add1 extends React.Component {
       <div id='panel' className='Add1'>
         <h3>Add1</h3>
 
+        <p>Sint nisi roof party excepteur sartorial. Shoreditch VHS nulla blog. Tote bag cliche lo-fi eiusmod bushwick, echo park shabby chic tousled austin placeat. Mixtape messenger bag trust fund mumblecore. Voluptate portland hella affogato. Venmo vegan health goth, truffaut echo park humblebrag accusamus kitsch post-ironic dolor. Affogato kombucha VHS, tote bag duis voluptate stumptown.</p>
+
         
       </div>
     )
--- client/modules/arithmetic/components/add3.jsx
+++ client/modules/arithmetic/components/add3.jsx
@@ -6,6 +6,8 @@ class Add3 extends React.Component {
     return (
       <div id='panel' className='Add3'>
         <h3>Add3</h3>
+
+        <p>Sartorial mustache mumblecore letterpress hammock +1. Brunch you probably haven't heard of them raw denim dreamcatcher 8-bit. Shoreditch forage mixtape, mustache heirloom flexitarian butcher yr intelligentsia asymmetrical listicle fanny pack retro. Pabst etsy neutra yuccie. Franzen helvetica actually sustainable, lo-fi banh mi 3 wolf moon church-key farm-to-table whatever tofu roof party lumbersexual celiac migas. Selfies cardigan salvia, wolf organic YOLO waistcoat shabby chic banh mi bicycle rights. Dreamcatcher mixtape asymmetrical sartorial small batch XOXO.</p>
       </div>
     )
   }
