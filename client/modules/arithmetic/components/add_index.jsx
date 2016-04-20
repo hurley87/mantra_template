fb50019e7389095e96ec0091a8d3c2d379cb7b21
hurley87 <dhurls99@gmail.com>
2016-04-19 21:08:48 -0400

    add index layout

--- client/modules/arithmetic/components/add_index.jsx
+++ client/modules/arithmetic/components/add_index.jsx
@@ -1,41 +1,8 @@
 import React from 'react';
+import IndexLayout from './index_layout.jsx'
 
 const AddIndex = ({additions}) => (
-  <div className="content-wrapper">
-    <div className="sidebar-toggler visible-xs">
-      <i className="ion-navicon"></i>
-    </div>
-    <div className="header">
-      Addition Exercises
-    </div>
-    <div className="steps">
-      <div className="step done">
-        <div className="info">
-          <span className="number">
-            <i className="ion-checkmark-circled"></i>
-          </span> 
-          This is an example of a module that has been completed
-        </div>
-        <a href="#" className="disabled button">
-          <span>Start</span>
-        </a>
-      </div>
-      {
-        additions.map((item, index) => (
-          <div key={item._id} className="step">
-            <div className="info">
-              <span className="number">{index + 1}</span> 
-              {item.title}
-            </div>
-
-            <a href={`/addition/${item._id}`} className="button">
-              <span>Start</span>
-            </a>
-          </div>
-        ))
-      }
-    </div>
-  </div>
+  <IndexLayout questions={additions} title='Addition Exercises'/>
 );
 
 export default AddIndex;
---
+++ client/modules/arithmetic/components/index_layout.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Row, Col, Input, Grid } from 'react-bootstrap';
+
+class IndexLayout extends React.Component {
+  render() {
+    return (
+      <div className="content-wrapper">
+        <div className="sidebar-toggler visible-xs">
+          <i className="ion-navicon"></i>
+        </div>
+        <div className="header">
+          {this.props.title}
+        </div>
+        <div className="steps">
+          <div className="step done">
+            <div className="info">
+              <span className="number">
+                <i className="ion-checkmark-circled"></i>
+              </span> 
+              This is an example of a module that has been completed
+            </div>
+            <a href="#" className="disabled button">
+              <span>Start</span>
+            </a>
+          </div>
+          {
+            this.props.questions.map((question, index) => (
+              <div key={question._id} className="step">
+                <div className="info">
+                  <span className="number">{index + 1}</span> 
+                  {question.title}
+                </div>
+
+                <a href={`/addition/${question._id}`} className="button">
+                  <span>Start</span>
+                </a>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default IndexLayout;
