1dc3161a5a7f64e0f4ec00eebbedf2b47cae1d6c
hurley87 <dhurls99@gmail.com>
2016-04-25 09:27:46 -0400

    show levels that are complete depending on how many points a user had

--- client/modules/arithmetic/containers/IndexLayout.js
+++ client/modules/arithmetic/containers/IndexLayout.js
@@ -3,9 +3,13 @@ import {useDeps, composeAll, composeWithTracker, compose} from 'mantra-core';
 import IndexLayout from '../components/IndexLayout.jsx';
 
 export const composer = ({context}, onData) => {
-  const {LocalState} = context();
+  const {LocalState, Meteor, Collections} = context();
   const toggle = LocalState.get('TOGGLE');
-  onData(null, {toggle});
+  const userId = Meteor.userId();
+  if(userId && Meteor.subscribe('profiles.single', userId).ready()){
+    const profile = Collections.Profiles.find({"user": userId}).fetch()[0];
+    onData(null, {toggle, profile})
+  }
 };
 
 export const depsMapper = (context, actions) => ({
