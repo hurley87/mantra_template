cd0c7209e0830e22914cb8e8ef4330c7a6fc6d93
hurley87 <dhurls99@gmail.com>
2016-04-25 09:27:46 -0400

    show levels that are complete depending on how many points a user had

--- client/modules/arithmetic/components/IndexLayout.jsx
+++ client/modules/arithmetic/components/IndexLayout.jsx
@@ -12,34 +12,33 @@ class IndexLayout extends React.Component {
           {this.props.title}
         </div>
         <div className="steps">
-          <div className="step done">
-            <div className="info">
-              <span className="number">
-                <i className="ion-checkmark-circled"></i>
-              </span> 
-              This is an example of a module that has been completed
-            </div>
-            <a href="#" className="disabled button">
-              <span>Start</span>
-            </a>
-          </div>
           {
             this.props.questions.map((question, index) => (
-              <div key={question._id} className="step">
+              <div key={question._id} className={ this.levelFinished(question.upperLimit) ? "step done" : "step"} >
                 <div className="info">
-                  <span className="number">{index + 1}</span> 
+                  <span className="number">
+                  { this.levelFinished(question.upperLimit) ? <i className="ion-checkmark-circled"></i> : index + 1 }
+                  </span> 
                   {question.title}
                 </div>
-
-                <a href={`/${this.props.type}/${question._id}`} className="button">
-                  <span>Start</span>
-                </a>
+                {this.startLevelBtn(this.props.type, question)}
               </div>
             ))
           }
         </div>
       </div>
     )
+  }
+  levelFinished(upperLimit) {
+    return this.props.profile.points > upperLimit;
+  }
+  startLevelBtn(type, question) {
+    // TODO: Lock levels depending on how many points a user has 
+    return (
+      <a href={`/${type}/${question._id}`} className="button">
+        { this.levelFinished(question.upperLimit) ? <span>Replay</span> : <span>Start</span> }
+      </a>
+    );
   }
 }
 
