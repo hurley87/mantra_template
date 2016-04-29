55333698790b472ee0edcfb273dbbcc7c55f8989
Tim Corcoran <tim.corcoran@utoronto.ca>
2016-04-29 13:50:29 -0400

    added improved billing component to accept credit card details

--- client/modules/dashboard/components/billing.jsx
+++ client/modules/dashboard/components/billing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Row, Col, Input, Grid } from 'react-bootstrap';
+import { Col, Row, Grid, Input, ButtonInput} from 'react-bootstrap';
+import { Form, ValidatedInput, RadioGroup, Radio } from 'react-bootstrap-validation';
 
 class Billing extends React.Component {
   render() {
@@ -25,82 +26,62 @@ class Billing extends React.Component {
 								</div>
 							</div>
 
-								<form id="billing" className="form-horizontal" method="post" action="#" role="form">
-					  			
-					  			<div className="form-group">
-						    		
-						    		<label className="col-sm-3 control-label">
-						    			Name on Card
-						    		</label>
-						    		
-						    		<div className="col-sm-9">
-						      			<input type="text" className="form-control" placeholder="Your full name" name="customer[first_name]" />
-						    		</div>
-					  			</div>
-					  	
-					  			<div className="address">
-					  				<div className="form-group">
-							    		<label className="col-sm-3 control-label">
-							    		Address
-							    		</label>
-							    	<div className="col-sm-9">
-							      		<input type="text" className="form-control" placeholder="Address" name="customer[address]" />
-							    	</div>
+							
+							<form id="billing" className="form-horizontal" method="post" action="#" role="form">
+							  	<div className="form-group">
+								    <label className="col-sm-3 control-label">Name on Card</label>
+								    <div className="col-sm-9">
+								      <input type="text" className="form-control" placeholder="Your full name" name="customer[first_name]" />
+								    </div>
+							  	</div>
+							  	<div className="address">
+							  		<div className="form-group">
+									    <label className="col-sm-3 control-label">Address</label>
+									    <div className="col-sm-9">
+									      	<input type="text" className="form-control" placeholder="Address" name="customer[address]" />
+									    </div>
+									</div>
+									<div className="form-group">
+									    <div className="col-sm-5 col-sm-offset-3">
+									      	<input type="text" className="form-control mobile-margin-bottom" placeholder="City" name="customer[city]" />
+									    </div>
+									    <div className="col-sm-4">
+									      	<input type="text" className="form-control" placeholder="Zip/Postal" name="customer[state]" />
+									    </div>
+								  	</div>
+								  	<div className="form-group">
+									    <div className="col-sm-5 col-sm-offset-3">
+									      	<input type="text" className="form-control mobile-margin-bottom" placeholder="Country" name="customer[city]" />
+									    </div>
+									    <div className="col-sm-4">
+									      	<input type="text" className="form-control" placeholder="State" name="customer[state]" />
+									    </div>
+								  	</div>
+							  	</div>
+							  	<div className="form-group">
+								    <label className="col-sm-3 control-label">Card Number</label>
+								    <div className="col-sm-9">
+								      <input type="text" className="form-control" placeholder="••••  ••••  ••••  ••••" name="customer[first_name]" />
+								    </div>
+							  	</div>
+							  	<div className="form-group">
+							  		<label className="col-sm-3 control-label">Expiration & CVC</label>
+								    <div className="col-sm-5">
+								      	<input type="text" className="form-control mobile-margin-bottom" placeholder="MM/YYY" name="customer[city]" />
+								    </div>
+								    <div className="col-sm-4">
+								      	<input type="text" className="form-control" placeholder="CVC" name="customer[state]" />
+								    </div>
+							  	</div>
+
+
+							  	<div className="action clearfix">
+									<a href="#" className="btn btn-success">
+										Make payment
+									</a>
 								</div>
-							
-								<div className="form-group">
-							    	<div className="col-sm-5 col-sm-offset-3">
-							      		<input type="text" className="form-control mobile-margin-bottom" placeholder="City" name="customer[city]" />
-							    	</div>
-							    
-							    	<div className="col-sm-4">
-							      		<input type="text" className="form-control" placeholder="Zip/Postal" name="customer[state]" />
-							    	</div>
-						  		</div>
-						  	
-						  		<div className="form-group">
-							    	<div className="col-sm-5 col-sm-offset-3">
-							      		<input type="text" className="form-control mobile-margin-bottom" placeholder="Country" name="customer[city]" />
-							    	</div>
-							    	
-							    	<div className="col-sm-4">
-							      		<input type="text" className="form-control" placeholder="State" name="customer[state]" />
-							    	</div>
-						  		</div>
-					  		</div>
-					  	
-					  		<div className="form-group">
-						    	<label className="col-sm-3 control-label">
-						    		Card Number
-						    	</label>
-						    
-						    	<div className="col-sm-9">
-						      		<input type="text" className="form-control" placeholder="••••  ••••  ••••  ••••" name="customer[first_name]" />
-						    	</div>
-					  		</div>
-					  	
-					  		<div className="form-group">
-					  			<label className="col-sm-3 control-label">
-					  				Expiration & CVC
-					  			</label>
-						    	
-						    	<div className="col-sm-5">
-						      		<input type="text" className="form-control mobile-margin-bottom" placeholder="MM/YYY" name="customer[city]" />
-						    	</div>
-						    
-						    	<div className="col-sm-4">
-						      		<input type="text" className="form-control" placeholder="CVC" name="customer[state]" />
-						    	</div>
-					  		</div>
-					  	
-							<div className="action clearfix">
-								<a href="#" className="btn btn-success">
-									Make payment
-								</a>
-							</div>
-							
 							</form>
-						
+									
 						</div>
 					</div>
 				</div>
@@ -109,7 +90,17 @@ class Billing extends React.Component {
 	</div>
     )
   }
+
+_handleValidSubmit(values) {
+	console.log("hello")
 }
+
+_handleInvalidSubmit(errors, values) {
+	console.log(errors)
+  }
+
+}
+
 
 
 export default Billing;
