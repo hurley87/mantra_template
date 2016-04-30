b320516097103842040fec0ef3b3b03b69a59fc1
hurley87 <dhurls99@gmail.com>
2016-04-30 12:36:36 -0400

    change homeapage around

--- client/modules/marketing/components/homepage.jsx
+++ client/modules/marketing/components/homepage.jsx
@@ -76,11 +76,47 @@ class Homepage extends React.Component {
                 </div>
               </div>
 
-              <Tabs />
+              <div id="features">
+                <div className="container">
+                  <div className="row header">
+                    <div className="col-md-12">
+                      <h2>Need an easy way to customize your site?</h2>
+                      <p>React is perfect for novice developers and experts alike.</p>
+                    </div>
+                  </div>
+                  <div className="row feature">
+                    <div className="col-md-6 info">
+                      <h4>You don't need to have great technical experience to use your product.</h4>
+                      <p>
+                        Whether you want to fill this paragraph with some text like I'm doing right now, this place
+                        is perfect to describe some features or anything you want - React has a complete solution for you.
+                      </p>
+                    </div>
+                    <div className="col-md-6 image">
+                      <img src="images/feature1.png" className="img-responsive" alt="feature1" />
+                    </div>
+                  </div>
+                  <div className="divider"></div>
+                  <div className="row feature backwards">
+                    <div className="col-md-6 info">
+                      <h4>A fully featured well design template that works.</h4>
+                      <p>
+                        You have complete control over the look & feel of your website, we offer the best quality so you take your site up and running in no time.
+                      </p>
+                      <p>
+                        Write some text here to explain the features of your site or application, it
+                        has lots of uses.
+                      </p>
+                    </div>
+                    <div className="col-md-6 image">
+                      <img src="images/feature2.png" className="img-responsive" alt="feature2" />
+                    </div>
+                  </div>
+                </div>    
+              </div>
               <Pricing />
               <Benefits />
               <Mobile />
-              <Features />
               <div id="testimonials">
                 <div className="container">
                   <div className="row header">
@@ -125,16 +161,7 @@ class Homepage extends React.Component {
                   </div>
                 </div>
               </div>
-
-              <div id="cta">
-                <p>
-                  Start your free 14 day trial! 
-                </p>
-                <a href="/register">
-                  Sign up for free
-                </a>
-              </div>
-
+              
               <div id="clients">
                 <div className="container">
                   <div className="row">
