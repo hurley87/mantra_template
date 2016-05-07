edee044bbc3b2d0bf4d46a0d9d524410a9f2914b
hurley87 <dhurls99@gmail.com>
2016-05-07 12:20:24 -0400

    bring counting points  into counting component

--- client/modules/arithmetic/containers/CountingIndex.js
+++ client/modules/arithmetic/containers/CountingIndex.js
@@ -5,7 +5,7 @@ export const composer = ({context}, onData) => {
   const {Meteor, Collections} = context();
   const userId = Meteor.userId();
   if(userId && Meteor.subscribe('profiles.single', userId).ready() && Meteor.subscribe('counting_questions.index').ready()){
-    const points = Collections.Profiles.find({"user": userId}).fetch()[0].addPoints;
+    const points = Collections.Profiles.find({"user": userId}).fetch()[0].countPoints;
     const questions = Collections.ArithmeticQuestions.find({}).fetch();
     onData(null, {points, questions})
   }
