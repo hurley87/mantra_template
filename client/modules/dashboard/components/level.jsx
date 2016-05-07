d1b0f47f322dabd7ba56d379900ff189e17ac674
hurley87 <dhurls99@gmail.com>
2016-05-07 17:27:33 -0400

    style the app inner

--- client/modules/dashboard/components/level.jsx
+++ client/modules/dashboard/components/level.jsx
@@ -6,8 +6,6 @@ class Level extends React.Component {
     return (
       <div className='step'>
         <div className='info'>
-          <span className='number'>
-          {this.props.points} <i className="fa fa-heart"></i></span>
           {this.props.title}
         </div> 
         <a href={this.props.link} className='button'> Start </a> 
