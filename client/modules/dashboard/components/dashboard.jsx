bf2dc9a65f87e11f610277bb6b8e0fae0d3c8307
hurley87 <dhurls99@gmail.com>
2016-05-07 11:59:56 -0400

    refactor dashboard

--- client/modules/dashboard/components/dashboard.jsx
+++ client/modules/dashboard/components/dashboard.jsx
@@ -1,57 +1,18 @@
 import React from 'react';
 import { Row, Col, Input, Grid } from 'react-bootstrap';
+import Metrics from './metrics.jsx'
+import Level from './level.jsx'
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div id='panel' className='dashboard'>
-        <div className="metrics clearfix">
-          <div className="metric">
-            <span className="field">Points</span>
-            <span className="data">{this.props.points}</span>
-          </div>
-          <div className="metric">
-            <span className="field">Right</span>
-            <span className="data">{this.props.profile.right}</span>
-          </div>
-          <div className="metric">
-            <span className="field">Wrong</span>
-            <span className="data">{this.props.profile.wrong}</span>
-          </div>
-          <div className="metric">
-            <span className="field">Ratio</span>
-            <span className="data">{this.props.ratio}</span>
-          </div>
-        </div>
+        <Metrics points={this.props.points} profile={this.props.profile} ratio={this.props.ratio} />
         <div className='steps'>
-          <div className='step'>
-            <div className='info'>
-              <span className='number'>{this.props.profile.addPoints} <i className="fa fa-heart"></i></span>
-              Addition
-            </div> 
-            <a href='/addition' className='button'> Start </a> 
-          </div>
-          <div className='step'>
-            <div className='info'>
-              <span className='number'>{this.props.profile.subPoints} <i className="fa fa-heart"></i></span>
-              Subtraction
-            </div>  
-            <a href='/subtraction' className='button'> Start </a> 
-          </div>
-          <div className='step'>
-            <div className='info'>
-              <span className='number'>{this.props.profile.multiPoints} <i className="fa fa-heart"></i></span>
-              Multiplication
-            </div> 
-            <a href='/multiplication' className='button'> Start </a> 
-          </div>
-          <div className='step'>
-            <div className='info'>
-              <span className='number'>{this.props.profile.divPoints} <i className="fa fa-heart"></i></span>
-              Division
-            </div> 
-            <a href='/division' className='button'> Start </a> 
-          </div>
+          <Level points={this.props.profile.addPoints} title='Addition' link='/addition' />
+          <Level points={this.props.profile.subPoints} title='Subtraction' link='/subtraction' />
+          <Level points={this.props.profile.multiPoints} title='Multiplication' link='/multiplication' />
+          <Level points={this.props.profile.divPoints} title='Division' link='/division' />
         </div>
       </div>
     )
