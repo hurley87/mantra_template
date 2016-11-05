6e3baa12fdbf800c5bc083a7d6594967b7a37d93
david hurley <davidhurley@davids-MacBook-Air.local>
2016-11-05 16:47:26 -0400

    cool

--- client/modules/users/actions/user.js
+++ client/modules/users/actions/user.js
@@ -1,5 +1,6 @@
 export default {
   createStudent({Meteor, LocalState, FlowRouter }, name, email, password, reward) {
+    console.log(name, email, password, reward)
     FlowRouter.go('/loading');
     console.log(name)
     Accounts.createUser({
