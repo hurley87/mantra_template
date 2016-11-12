80b6ee1d63880d9bd6f371e8a7c6659c204096fa
david hurley <davidhurley@davids-MacBook-Air.local>
2016-11-12 15:18:38 -0500

    bug fixin' '

--- client/modules/users/actions/login.js
+++ client/modules/users/actions/login.js
@@ -4,11 +4,13 @@ export default {
     Meteor.loginWithPassword(email, password, function(error){
       if(error) {
         LocalState.set('LOGIN_USER', 'Incorrect login. Check your email and password.')
+        FlowRouter.go("/login")
       } else {
         if(Meteor.userId()){
           FlowRouter.go("/challenges")
         }else{
           LocalState.set('LOGIN_USER', 'Incorrect login. Check your email and password.')
+          FlowRouter.go("/login")
         }
       }
     });
@@ -18,11 +20,13 @@ export default {
     Meteor.loginWithPassword(username, password, function(error){
         if(error) {
           LocalState.set('LOGIN_USER', 'Incorrect login. Check your username and password.')
+          FlowRouter.go("/login")
         } else {
           if(Meteor.userId()){
             FlowRouter.go("/challenges")
           }else{
             LocalState.set('LOGIN_USER', 'Incorrect login. Check your username and password.')
+            FlowRouter.go("/login")
           }
         }
     });
